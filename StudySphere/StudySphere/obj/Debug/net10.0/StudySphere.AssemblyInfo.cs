c056128b108fe45b8f4ff64018d90c9ba6374b01
Bhalodiya-khush <bhalodiyakhush9@gmail.com>
2026-09-17 01:00:44 +0530

    Repository pattern added for Instructor Dashboard

--- StudySphere/StudySphere/obj/Debug/net10.0/StudySphere.AssemblyInfo.cs
+++ StudySphere/StudySphere/obj/Debug/net10.0/StudySphere.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudySphere")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1db014eb321f13143f31a8657dfaf5bc71d1277")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudySphere")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudySphere")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
